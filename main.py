6729cb2c01521864245ded59b8293d7023bc7f4b
ArtemLevin <python_study_2022@mail.ru>
2023-08-04 16:28:27 +0300

    added memoization

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import functools
 @functools.lru_cache()
 def fib(number):
-    if number == 1 or number == 0:
+     if number == 1 or number == 2:
+         return 1
+     else:
+         return fib(number-1) + fib(number-2)
+
+print(fib(50))
+
+fibonacciCache = {}
+
+def fibonacci(nthNumber):
+    global fibonacciCache
+    if nthNumber in fibonacciCache:
+        return fibonacciCache[nthNumber]
+
+    if nthNumber == 1 or nthNumber == 2:
+        fibonacciCache[nthNumber] = 1
         return 1
     else:
-        return fib(number-1) + fib(number-2)
+        result = fibonacci(nthNumber-1)
+        result = result + fibonacci(nthNumber- 2)
+        fibonacciCache[nthNumber] = result
+        return result
 
-print(fib(150))
+print(fibonacci(50))
