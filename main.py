71d4f310e4da65b7a3211268c2cc9487eba0b443
ArtemLevin <python_study_2022@mail.ru>
2023-08-04 16:17:05 +0300

    added functools.lru_cache

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import functools
+@functools.lru_cache()
 def fib(number):
     if number == 1 or number == 0:
         return 1
     else:
         return fib(number-1) + fib(number-2)
 
-print(fib(5))
+print(fib(15))
